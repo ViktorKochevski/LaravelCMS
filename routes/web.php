bb7ac4c6311c64842e4f367c2e3c5049f6088f9e
Viktor <viktorkochevski.vk@gmail.com>
2023-02-24 19:57:40 +0100

    Creating database migration for countries Creating seeder for countries with all countries creating Country model

--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,14 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::middleware(['web', 'auth'])->prefix('dashboard')->group(function () {
-
+    Route::get('/users', [App\Http\Controllers\UserController::class, 'index'])->name('users.index');
+    Route::get ('/users/create', [App\Http\Controllers\UserController::class, 'create'])->name('users.create');
+    Route::post('/users', [App\Http\Controllers\UserController::class, 'store'])->name('users.store');
+    Route::get('/user/{user}', [App\Http\Controllers\UserController::class, 'show'])->name('users.show');
+    Route::get('/user/{user}/edit', [App\Http\Controllers\UserController::class, 'edit'])->name('users.edit');
+    Route::put('/user/{user}', [App\Http\Controllers\UserController::class, 'update'])->name('users.update');
+    Route::delete('/user/{user}', [App\Http\Controllers\UserController::class, 'destroy'])->name('users.destroy');
 });
+
+
+
